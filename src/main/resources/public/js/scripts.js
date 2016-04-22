43b62d43d72b321c03c77af0a93707ed7ba43c21
Patrick Wells <patmicwells@gmail.com>
2016-04-22 14:48:49 -0700

    include a link to the root page so that the user can return to the homepage so that they can play again.

--- src/main/resources/public/js/scripts.js
+++ src/main/resources/public/js/scripts.js
@@ -14,14 +14,29 @@ function cleanText(array){
 
 $(document).ready(function(){
 
-  var javaOutput = $("#gameOutput")[0].innerText.split("");
+  var javaOutput = $(".gameOutput")[0].innerText.split("");
   var userInputArray = $(".userInputValue")[0].innerText.split("");
   var cleanUserInputArray = cleanText(userInputArray);
 
-  $("#gameOutput").empty();
+  for(i = 0; i < javaOutput.length; i++){
+    if(javaOutput[i] == " "){
+      cleanUserInputArray.splice(i, 0, " ");
+    }
+  }
+
+  $(".gameOutput").empty();
+
 
   for(i = 0; i < javaOutput.length; i++){
-    $("#gameOutput").append("<span class = 'letters'>" + javaOutput[i] + "</span>");
+    var classLetters = "";
+
+    if(javaOutput.length > 15){
+      $(".gameOutput").attr("id", "longWord");
+    }
+    if(javaOutput[i] !== " "){
+      classLetters = " boxBackground";
+    }
+    $(".gameOutput").append("<span class = 'letters" + classLetters + "'>" + javaOutput[i] + "</span>");
   }
 
   $("span").click(function(){
