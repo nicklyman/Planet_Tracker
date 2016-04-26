23daf32af96db685fc8b414f3995ce3b91ab31c9
Patrick Wells <patmicwells@gmail.com>
2016-04-26 06:13:56 -0700

    Build template file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,29 +11,7 @@ import static spark.Spark.*;
 public class App {
 
   public static void main(String[] args) {
-    staticFileLocation("/public");
-    String layout = "templates/layout.vtl";
-
-    get("/", (request, response) -> {
-      HashMap model = new HashMap();
-
-      model.put("template", "templates/home.vtl");
-      
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
-
-    post("/results", (request, response) -> {
-      HashMap model = new HashMap();
-      ReplaceVowels newReplace = new ReplaceVowels();
-      String userInput = request.queryParams("vowelGame");
-
-      model.put("userInput", userInput);
-      model.put("gameString", newReplace.vowelRemover(userInput));
-      model.put("template", "templates/results.vtl");
-
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
-
+    
   }
 
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -16,20 +16,20 @@ public class AppTest extends FluentTest {
 
   @ClassRule
   public static ServerRule server = new ServerRule();
-
-  @Test
-  public void rootTest() {
-    goTo("http://localhost:4567/");
-    assertThat(pageSource()).contains("Welcome to the Vowel Game!");
-  }
-
-  @Test
-  public void VowelsRemoved() {
-    goTo("http://localhost:4567");
-    fill("#vowelGame").with("Epicodus");
-    submit("button");
-    assertThat(pageSource()).contains("-p-c-d-s");
-  }
+  // 
+  // @Test
+  // public void rootTest() {
+  //   goTo("http://localhost:4567/");
+  //   assertThat(pageSource()).contains("Welcome to the Vowel Game!");
+  // }
+  //
+  // @Test
+  // public void VowelsRemoved() {
+  //   goTo("http://localhost:4567");
+  //   fill("#vowelGame").with("Epicodus");
+  //   submit("button");
+  //   assertThat(pageSource()).contains("-p-c-d-s");
+  // }
 
 
 }
