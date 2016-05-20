64125b1c4e8819fcdc1d901f26dacedf1062d702
blessb15 <blakebless7@gmail.com>
2016-05-20 09:34:11 -0700

    DB.java tweak.

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -2,5 +2,5 @@ import org.sql2o.*;
 
 public class DB {
   public static Sql2o sql2o = new
-  Sql2o("jdbc:postgresql://localhost:5432/planet_tracker", user, pass);
+  Sql2o("jdbc:postgresql://localhost:5432/planet_tracker", null, null);
 }
